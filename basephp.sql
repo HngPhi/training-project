d2893ca7cfd6ce658b6e6a99d292d2ad6ce2cd2f
HngPhi <pigboss2507@gmail.com>
2021-12-28 21:24:13 +0700

    Add file sql

---
+++ basephp.sql
@@ -0,0 +1,70 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : demo
+ Source Server Type    : MySQL
+ Source Server Version : 100422
+ Source Host           : localhost:3306
+ Source Schema         : basephp
+
+ Target Server Type    : MySQL
+ Target Server Version : 100422
+ File Encoding         : 65001
+
+ Date: 28/12/2021 21:20:49
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for admin
+-- ----------------------------
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `avatar` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `role_type` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '1' COMMENT '1/ Super admin, \r\n2/ Admin',
+  `ins_id` int NOT NULL,
+  `upd_id` int NULL DEFAULT NULL,
+  `ins_datetime` datetime(0) NOT NULL,
+  `upd_datetime` datetime(0) NULL DEFAULT NULL,
+  `del_flag` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0' COMMENT '0/ Active, \r\n1/ Deleted',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of admin
+-- ----------------------------
+INSERT INTO `admin` VALUES (1, 'Admin1', 'admin123', 'admin@gmail.com', '', '1', 0, NULL, '2021-12-24 14:04:03', NULL, '0');
+INSERT INTO `admin` VALUES (2, 'SuperAdmin', 'superadmin123', 'superadmin@gmail.com', '', '2', 0, NULL, '2021-12-24 15:04:42', NULL, '0');
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `facebook_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `avatar` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `status` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '1' COMMENT '1/ Active, 2/ Banned',
+  `ins_id` int NOT NULL,
+  `upd_id` int NULL DEFAULT NULL,
+  `ins_datetime` datetime(0) NOT NULL,
+  `upd_datetime` datetime(0) NULL DEFAULT NULL,
+  `del_flag` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0' COMMENT '0/ Active, 1/ Deleted',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, 'Nguyễn Văn A', '1', 'nguyenvana@gmail.com', '', '1', 0, NULL, '2021-12-24 13:58:49', NULL, '0');
+INSERT INTO `user` VALUES (2, 'Nguyễn Văn B', '2', 'nguyenvanb@gmail.com', '', '1', 0, NULL, '2021-12-24 14:00:27', NULL, '0');
+
+SET FOREIGN_KEY_CHECKS = 1;
